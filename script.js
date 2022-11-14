ba25a70eb7271ca3414983833225100db0b7400b
johannaeliise <johannaeliise.teder@wsu.edu>
2022-11-14 07:27:22 +0000

    final update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /* Javascript code for Tiny project #8 - js */
 cardsClicked = 0;
 
-function cardClicked(what){
+function cardClicked(what) {
 
   if (!what.classList.contains("removed")) {
 
     if (what.classList.contains("clicked")) {
-    
+
       what.classList.remove("clicked");
       cardsClicked--;
 
@@ -18,102 +18,104 @@ function cardClicked(what){
       if (cardsClicked == 2) {
         // compare card values
         cardCompare();
-      } 
-    } 
+      }
+    }
   }
 }
-      function cardCompare() {
-          clickedCards = document.querySelectorAll(".clicked"); //collection of cards
 
-          //first clicked element is clickedCrds[0]
-          //second clicked element is clickedCards[1]
-      matched = false; 
+function cardCompare() {
+  clickedCards = document.querySelectorAll(".clicked"); //collection of cards
 
-            if (clickedCards[0].classList.contains("pic1") && clickedCards[1].classList.contains("pic1")) {
-              matched = true; //matches pic #1
-            } else if (clickedCards[0].classList.contains("pic2") && clickedCards[1].classList.contains("pic2")) {
-              matched = true; //matches pic #2
-            } else if (clickedCards[0].classList.contains("pic3") && clickedCards[1].classList.contains("pic3")) {
-              matched = true; //matches pic #3
-            } else if (clickedCards[0].classList.contains("pic4") && clickedCards[1].classList.contains("pic4")) {
-              matched = true; //matches pic #4
-            } else if (clickedCards[0].classList.contains("pic5") && clickedCards[1].classList.contains("pic5")) {
-              matched = true; //matches pic #5
-            } else if (clickedCards[0].classList.contains("pic6") && clickedCards[1].classList.contains("pic6")) {
-              matched = true; //matches pic #6
-            } else if (clickedCards[0].classList.contains("pic7") && clickedCards[1].classList.contains("pic7")) {
-              matched = true; //matches pic #7
-            } else if (clickedCards[0].classList.contains("pic8") && clickedCards[1].classList.contains("pic8")) {
-              matched = true; //matches pic #8
-            } else if (clickedCards[0].classList.contains("pic9") && clickedCards[1].classList.contains("pic9")) {
-              matched = true; //matches pic #9
-            } else if (clickedCards[0].classList.contains("pic10") && clickedCards[1].classList.contains("pic10")) {
-              matched = true; //matches pic #10
-            }
+  //first clicked element is clickedCrds[0]
+  //second clicked element is clickedCards[1]
+  matched = false;
 
-            if (matched) {
-              //hide cards
-              removeCards(clickedCards[0], clickedCards[1]);
+  if (clickedCards[0].classList.contains("pic1") && clickedCards[1].classList.contains("pic1")) {
+    matched = true; //matches pic #1
+  } else if (clickedCards[0].classList.contains("pic2") && clickedCards[1].classList.contains("pic2")) {
+    matched = true; //matches pic #2
+  } else if (clickedCards[0].classList.contains("pic3") && clickedCards[1].classList.contains("pic3")) {
+    matched = true; //matches pic #3
+  } else if (clickedCards[0].classList.contains("pic4") && clickedCards[1].classList.contains("pic4")) {
+    matched = true; //matches pic #4
+  } else if (clickedCards[0].classList.contains("pic5") && clickedCards[1].classList.contains("pic5")) {
+    matched = true; //matches pic #5
+  } else if (clickedCards[0].classList.contains("pic6") && clickedCards[1].classList.contains("pic6")) {
+    matched = true; //matches pic #6
+  } else if (clickedCards[0].classList.contains("pic7") && clickedCards[1].classList.contains("pic7")) {
+    matched = true; //matches pic #7
+  } else if (clickedCards[0].classList.contains("pic8") && clickedCards[1].classList.contains("pic8")) {
+    matched = true; //matches pic #8
+  } else if (clickedCards[0].classList.contains("pic9") && clickedCards[1].classList.contains("pic9")) {
+    matched = true; //matches pic #9
+  } else if (clickedCards[0].classList.contains("pic10") && clickedCards[1].classList.contains("pic10")) {
+    matched = true; //matches pic #10
+  }
 
-            } else {
-              //unflip cards
-              unflipCards(clickedCards[0], clickedCards[1]);
+  if (matched) {
+    //hide cards
+    removeCards(clickedCards[0], clickedCards[1]);
 
-            }
-          }
+  } else {
+    //unflip cards
+    unflipCards(clickedCards[0], clickedCards[1]);
+
+  }
+}
 
 
-          function removeCards(cardA, cardB) {
-            pause = setTimeout(function() {
+function removeCards(cardA, cardB) {
+  pause = setTimeout(function() {
 
-              cardA.classList.remove("clicked");
-              cardB.classList.remove("clicked");
+    cardA.classList.remove("clicked");
+    cardB.classList.remove("clicked");
 
-              cardA.classList.add("removed");
-              cardB.classList.add("removed");
+    cardA.classList.add("removed");
+    cardB.classList.add("removed");
 
-              cardsClicked = 0;
+    cardsClicked = 0;
 
-              checkWinning(); }, 500);
-          }
+    checkWinning();
+  }, 500);
+}
 
-          function unflipCards(cardA, cardB) {
+function unflipCards(cardA, cardB) {
 
-            pause = setTimeout(function() {
-              cardA.classList.remove("clicked");
-              cardB.classList.remove("clicked");
+  pause = setTimeout(function() {
+    cardA.classList.remove("clicked");
+    cardB.classList.remove("clicked");
 
-              cardsClicked = 0;
-            }, 500);
-          }
+    cardsClicked = 0;
+  }, 500);
+}
 
-          function checkWinning() {
-            remainingCards = document.querySelectorAll(".card");
+function checkWinning() {
+  remainingCards = document.querySelectorAll(".card");
 
-            for (c = 0; c < remainingCards.length; c++) {
-              if (!remainingCards[c].classList.contains("removed")) {
-                return;
-              }
-            }
-            document.getElementById("mainTable").innerHTML = "You have won the game."
-          }
+  for (c = 0; c < remainingCards.length; c++) {
+    if (!remainingCards[c].classList.contains("removed")) {
+      return;
+    }
+  }
+  document.getElementById("mainTable").innerHTML = "You have won the game."
+}
 
-          function shuffleCards() {
-            table = document.querySelector("#mainTable");
-            cardCount = table.children.length;
+function shuffleCards() {
+  table = document.querySelector("#mainTable");
+  cardCount = table.children.length;
 
-            for (c = 0; c < cardCount; c++) {
-              randomCard = Math.floor(Math.random() * cardCount);
-              cardToMove = table.children[randomCard];
-              table.appendChild(cardToMove);
-            }
-          }
+  for (c = 0; c < cardCount; c++) {
+    randomCard = Math.floor(Math.random() * cardCount);
+    cardToMove = table.children[randomCard];
+    table.appendChild(cardToMove);
+  }
+}
 
 
 
 window.onload = function() {
   shuffleCards();
-  
+
   cardList = document.querySelectorAll(".card"); // collection of cards
 
   cardCount = cardList.length; // how many cards are on the table
