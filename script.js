46354ad09a0fc2593ed727a806c344beb661ab5c
johannaeliise <johannaeliise.teder@wsu.edu>
2022-11-14 04:59:49 +0000

    basic clicks

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* Javascript code for Tiny project #8 - js */
+
+windonw.onload = function () {
+  
+  cardList = document.querySelectorAll(“.card”); //collection of cards 
+
+cardCount = cardList.length 
+
+for (c=0; c < cardCount; c++) {
+  cardList[c].onclick = function () {
+  this.classList.toggle(“clicked”);
+}
+}
+}
